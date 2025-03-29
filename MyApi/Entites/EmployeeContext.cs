77ee91aa2c91f93d3b48051765fe3c50d919c1e7
tanaton akko <tanaton@tanatons-MacBook-Air.local>
2025-03-29 17:50:45 +0700

    init commit

---
+++ MyApi/Entites/EmployeeContext.cs
@@ -0,0 +1,8 @@
+public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
